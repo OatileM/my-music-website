a2786f6dae38e7acfe3852fb3ec6bf86527de338
Oatile Mdlela <osmdlela@gmail.com>
2024-01-06 01:40:39 +0200

    Home screen has been updated.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MusicComponentComponent  } from './music-component/music-component.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
+  { path: '', component: HomeComponent  },
   { path: 'music', component: MusicComponentComponent  }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MusicComponentComponent } from './music-component/music-component.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MusicComponentComponent
+    MusicComponentComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
